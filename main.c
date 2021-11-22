38c18e6abad6ab87cac58ad15db9d6a72566a47c
BarNahmias <92825016+BarNahmias@users.noreply.github.com>
2021-11-22 16:07:47 +0200

    Add files via upload

--- main.c
+++ main.c
@@ -1,14 +1,11 @@
 #include "my_mat.h"
-
 #include <stdio.h>
-
 #define N 10
+
 
 int  matrix [N][N];
 
 int main(){
-
-
 char dig;
 
 while( dig!='D'){
@@ -16,11 +13,17 @@ scanf("%c",&dig);
 
 switch (dig)
 {
-case 'A' : newMatrix(); break; //create new matrix from inpot 
-case 'B' : isExists(); break; break; //cheack if exists route(data) in location in [i][j]  and return true or false
-case 'C' : floydWarshall(); break; //return (int)  value from  route(data) in location in [i][j]   
-}
-}
+case 'A' :
+newMatrix();
+break; //create new matrix from inpot 
+case 'B' :
+isExists();
+break; //cheack if exists route(data) in location in [i][j]  and return true or false
+case 'C' :
+floydWarshall();
+break; //return (int)  value from  route(data) in location in [i][j]   
+        }
+    }
 }
 
 
