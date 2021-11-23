bd4b9c91c33117e0d6bb95768fc0bffe41d5ac7c
BarNahmias <92825016+BarNahmias@users.noreply.github.com>
2021-11-23 17:53:22 +0200

    Add files via upload

--- my_mat.c
+++ my_mat.c
@@ -15,22 +15,6 @@ for(int i=0;i<N;i++) {
    }
 }
 
-
-
-//cheack if exists route(data) in location in [i][j]
-// and return true or false
-void isExists(){
-    int i;
-    int j;
-    scanf("%d",&i);
-    scanf("%d",&j);
-    if(matrix[i][j]!=0){
-      printf("True\n");  
-    }else{ printf("False\n");}
-}
-
-
-
 //return (int)  value from  route(data) in location in [i][j]   
 int Route(){
     int i;
@@ -44,6 +28,36 @@ int Route(){
     }
     return -1;
     }
+
+//cheack if exists route(data) in location in [i][j]
+// and return true or false
+
+
+void isExists(){
+  int newMatrix[N][N], i, j, k;
+  int route;
+  for (i = 0; i < N; i++)
+    for (j = 0; j < N; j++)
+      newMatrix[i][j] = matrix[i][j];
+
+  // Adding vertices individually
+  for (k = 0; k < N; k++){
+    for (i = 0; i < N; i++){
+      for (j = 0; j < N; j++){
+        if (newMatrix[i][k] + newMatrix[k][j] < newMatrix[i][j])
+          newMatrix[i][j] = newMatrix[i][k] + newMatrix[k][j];
+      }
+    }
+  }route = Route(newMatrix);
+  if(route!= -1 ){
+    printf("True\n");}
+    else {printf("False\n");} 
+}
+
+
+
+
+
 
 // Implementing floyd warshall algorithm
 void floydWarshall(){
